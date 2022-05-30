2d94d16f3fe95de86b41f4b1274bf996a08312f0
aplusiv <50slash50@gmail.com>
2022-05-30 12:54:16 +0300

    second commit

--- src/js/CharacterClass.js
+++ src/js/CharacterClass.js
@@ -1,28 +1,19 @@
-/* import Bowman from "./BowmanClass";
-import Swordsman from "./SwordsmanClass";
-import Magician from "./MagicianClass";
-import Undead from "./UndeadClass";
-import Zombie from "./ZombieClass";
-import Daemon from "./DaemonClass"; */
-
 export default class Character {
-  constructor(name, type, attack, defence) {
+  constructor(name, type) {
     if (typeof name === 'string' && name.length >= 2 && name.length <= 10) {
       this.name = name;
     } else {
       throw new Error('Wrong name, try again');
     }
-    if (typeof type === 'string' && (type === 'Bowman' || type === 'Swordsman' || type === 'Magician' || type === 'Daemon' || type === 'Undead' || type === 'Zombie')) {
+    if (typeof type === 'string' && ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(type)) {
       this.type = type;
     } else {
       throw new Error('Wrong type, type not found, try again');
     }
     this.health = 100;
     this.level = 1;
-    this.attack = attack;
-    this.defence = defence;
-    /* По заданию должны быть эти 4 свойства в классе Character,
-    но не проще ли их было добавлять наследникам в конструктор? */
+    this.attack = undefined;
+    this.defence = undefined;
   }
 
   levelUp() {
@@ -37,7 +28,7 @@ export default class Character {
   }
 
   damage(points) {
-    if (this.health >= 0) {
+    if (this.health > 0) {
       this.health -= points * (1 - this.defence / 100);
     } else {
       throw new Error("It's denied. Your health is 0 or less. You're dead");
--- src/js/tests/CharacterClass.test.js
+++ src/js/tests/CharacterClass.test.js
@@ -1,24 +1,24 @@
 import Character from '../CharacterClass';
 
-// Может как-то нужно оптимизировать тесты? Объединить их
-
 test.each([
   [4, new Error('Wrong name, try again')],
   ['A', new Error('Wrong name, try again')],
   ['StanlyManly', new Error('Wrong name, try again')],
 ])('should throw new Error "Wrong name, try again" if there is incorrect parameter', (name, error) => {
-  expect(() => new Character(name, 'Bowman', 50, 80)).toThrowError(error);
+  expect(() => new Character(name, 'Bowman')).toThrowError(error);
 });
 
 test.each([
   [true, new Error('Wrong type, type not found, try again')],
   ['King', new Error('Wrong type, type not found, try again')],
 ])('should throw new Error "Wrong name, try again" if there is incorrect parameter', (type, error) => {
-  expect(() => new Character('John Doe', type, 50, 50)).toThrowError(error);
+  expect(() => new Character('John Doe', type)).toThrowError(error);
 });
 
 test("should upgrade character's level", () => {
-  const received = new Character('John Doe', 'Daemon', 10, 55);
+  const received = new Character('John Doe', 'Daemon');
+  received.attack = 10;
+  received.defence = 55;
   const expected = {
     name: 'John Doe',
     type: 'Daemon',
@@ -35,14 +35,17 @@ test("should upgrade character's level", () => {
 });
 
 test("method levelUp() should should throw new Error It's denied. You're dead", () => {
-  const received = new Character('John Doe', 'Daemon', 10, 55);
-  received.health = 0; // Так искусственно можно делать в тестах?
+  const received = new Character('John Doe', 'Daemon');
+  received.health = 0;
 
   expect(() => received.levelUp()).toThrowError(new Error("It's denied. You're dead"));
 });
 
 test("should refresh caracter's health if it was damaged", () => {
-  const received = new Character('John Doe', 'Daemon', 10, 55);
+  const received = new Character('John Doe', 'Daemon');
+  received.attack = 10;
+  received.defence = 55;
+
   const expected = {
     name: 'John Doe',
     type: 'Daemon',
@@ -57,8 +60,8 @@ test("should refresh caracter's health if it was damaged", () => {
 });
 
 test("metod damage(points) should throw new Error It's denied. Your health is 0 or less. You're dead", () => {
-  const received = new Character('John Doe', 'Daemon', 10, 55);
-  received.health = -5; // Так искусственно можно делать в тестах?
+  const received = new Character('John Doe', 'Daemon');
+  received.health = -5;
 
   expect(() => received.damage(25)).toThrowError(new Error("It's denied. Your health is 0 or less. You're dead"));
 });
